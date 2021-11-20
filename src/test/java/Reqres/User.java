4f4cfb31d39c3aec5a71109250907755177f612a
s.fedulov <s.fedulov@corp.vk.com>
2021-11-20 14:32:57 +0300

    Add new tests

--- src/test/java/Reqres/User.java
+++ src/test/java/Reqres/User.java
@@ -1,31 +1,65 @@
 package Reqres;
 
+import Reqres.models.UserData;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
+import static Reqres.Specs.request;
+import static Reqres.Specs.responseSpec;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
-import static Reqres.Specs.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class User {
     @Test
-    public void singleUser() {
-        given()
+    public void userJanetData() {
+        UserData data = given()
                 .spec(request)
                 .when()
                 .get("/users/2")
                 .then()
                 .spec(responseSpec)
-                .log().body();
+                .extract().as(UserData.class);
+
+        assertEquals(2, data.getUser().getId());
+        assertEquals("janet.weaver@reqres.in", data.getUser().getEmail());
+        assertEquals("Janet", data.getUser().getFirstName());
+        assertEquals("Weaver", data.getUser().getLastName());
+    }
+
+    @Test
+    public void userEmmaData() {
+        UserData data = given()
+                .spec(request)
+                .when()
+                .get("/users/3")
+                .then()
+                .spec(responseSpec)
+                .extract().as(UserData.class);
+
+        assertEquals(3, data.getUser().getId());
+        assertEquals("emma.wong@reqres.in", data.getUser().getEmail());
+        assertEquals("Emma", data.getUser().getFirstName());
+        assertEquals("Wong", data.getUser().getLastName());
     }
 
     @Test
     public void statusCodeTest() {
-        given()
+        UserData data = given()
                 .spec(request)
                 .get("/users?page=2")
                 .then()
                 .spec(responseSpec)
-                .body("total_pages", is(2));
+                .body("total_pages", is(2))
+                .extract().as(UserData.class);
+    }
+
+    @Test
+    public void noSuchUserTest() {
+        given()
+                .spec(request)
+                .get("/users/23")
+                .then()
+                .statusCode(404);
     }
 
     @Test
